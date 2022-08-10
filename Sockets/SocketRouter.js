532f7c2f26a0d8bee427a31d780996fa931aa47a
stan1i <alexandersamedov@gmail.com>
2022-08-10 12:07:57 +0300

    v0.96

--- Sockets/SocketRouter.js
+++ Sockets/SocketRouter.js
@@ -23,7 +23,7 @@ class SocketRouter{
                             user = tokenService.validationToken(data.token)
                             if(data.game){
                                 game = data.game
-                            }else{
+                            }else if(user.game){
                                 game = user.game
                                 data.game = game
                             }
