4a2095eda62d61eda84ce2130a7a79b6adb5a1d7
stan1i <alexandersamedov@gmail.com>
2022-08-17 11:38:44 +0300

    add key word 0.2

--- Sockets/Services/ControlGame.js
+++ Sockets/Services/ControlGame.js
@@ -47,6 +47,7 @@ class ControlGame{
         this._sendMessage(DataCollection.getGameData( this.playersWs, this), 'all')
 
         this._sendMessage( {action:'teamsMap', tasks:this.roundTasks}, 'admin')
+        this._sendMessage( {action:'keyWords', words:this.keyWords}, 'admin')
         this._sendDispositionTeams()
     }
     stopGame(){
@@ -232,6 +233,7 @@ class ControlGame{
     connectAdmin(){
         this._sendMessage( {action:'teamsMap', tasks:this.isEvent?this.eventList.find(event=>!event.done && event.round === this.currentRound + 1):this.roundTasks}, 'admin')
         this._sendMessage( {action:'event', events:this.eventList}, 'admin')
+        this._sendMessage( {action:'keyWords', words:this.keyWords}, 'admin')
     }
 
 //----------------Управление раундами игры
